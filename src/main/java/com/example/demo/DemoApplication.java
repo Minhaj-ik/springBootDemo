a51c57e7e103ef1ef229e807c9a2022316bd6711
Minhaj I K <minhajismailkhan@gmail.com>
2023-11-06 12:06:56 +0530

    just have some demo of spring ioc, di

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-		System.out.print("Its is working>>>>>>>>>>");
+		ConfigurableApplicationContext ct= SpringApplication.run(DemoApplication.class, args);
+		System.out.print("Its is working  >>>>>>>>>>");
+		
+		Alien a= ct.getBean(Alien.class);
+		a.show();
 	}
 
 }
